dcab7aaaf58792f40677e69bc609023ddd1389e9
Kiran Data Analyst <157777144+obitorin07@users.noreply.github.com>
2025-01-28 14:38:20 +0530

    Create timetravel.sql

---
+++ time_travel/timetravel.sql
@@ -0,0 +1,6 @@
+
+create or replace table time_travel ( jobod varchar(25) ,company_name varchar(25))
+data_retention_time_in_days=1; 
+
+-- 1 means 24hours 0 means off and if we add 1> it will not allow because 
+--i'm using trial version so 24hours only i can time travel and 
